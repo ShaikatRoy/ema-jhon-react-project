63c6e477c8f4f45bf94696ed80a3f60aac8aaffd
Shaikat roy <shaikatroy258@gmail.com>
2023-03-29 00:57:21 +0600

    cart added

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -4,12 +4,19 @@ import './Shop.css'
 
 const Shop = () => {
 const [products, setProducts] = useState([]);
+const [cart, setCart] = useState([])
 
 useEffect( () =>{
     fetch('https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json')
     .then(res => res.json())
     .then(data => setProducts(data))
 }, [])
+
+const handleAddToCart = (product) => {
+    // console.log(product)
+    const newCart = [...cart, product];
+    setCart(newCart);
+}
 
     return (
         <div className='shop-container'>
@@ -19,12 +26,14 @@ useEffect( () =>{
                     products.map(product => <Product
                     key={product.id}
                     product={product}
+                    handleAddToCart={handleAddToCart}
                     />)
-                }
+                } 
             </div>
 
             <div className="cart-container">
                 <h4>Order Summary</h4>
+                <p>Selected Items: {cart.length}</p>
             </div>
 
         </div>
